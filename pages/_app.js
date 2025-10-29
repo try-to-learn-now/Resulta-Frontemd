2aef603f4e5f7bb757125543ac7f1fa0777f8fa0
kushwaha jee <145318424+try-to-learn-now@users.noreply.github.com>
2025-10-29 19:53:26 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+// pages/_app.js
+import '../styles/globals.css'; // Import global styles
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
